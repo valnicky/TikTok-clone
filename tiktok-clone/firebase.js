8a88adb92792d8df8199015d105ce2f29b23101f
Valeria <valery0007@gmail.com>
2021-03-06 22:53:12 +0100

    config firebase

---
+++ tiktok-clone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCSowMvOvg3I23RzPFyJUq_6jDCzcjJ0lc",
+    authDomain: "tik-tok-clone-d1aed.firebaseapp.com",
+    projectId: "tik-tok-clone-d1aed",
+    storageBucket: "tik-tok-clone-d1aed.appspot.com",
+    messagingSenderId: "533338026382",
+    appId: "1:533338026382:web:05f566a3120d23a32ef4df",
+    measurementId: "G-TLDMKD6LJ8"
+};
